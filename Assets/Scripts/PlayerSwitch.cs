30439f15a23f20d2ab65eeb5ae69b96c0dae980a
dulak <140708360+Kubak2673@users.noreply.github.com>
2025-02-14 12:03:34 +0100

    fixes, improvements, scenes.

--- Assets/Scripts/PlayerSwitch.cs
+++ Assets/Scripts/PlayerSwitch.cs
@@ -1,37 +1,40 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.SceneManagement;
+using UnityEngine.Tilemaps;
 
 public class PlayerSwitch : MonoBehaviour
 {
-    [SerializeField] GameObject box1;
-    [SerializeField] GameObject box2;
+    // Movement
     [SerializeField] MonoBehaviour player1Movement;
     [SerializeField] MonoBehaviour player2Movement;
     [SerializeField] MonoBehaviour player1Position;
     [SerializeField] MonoBehaviour player2Position;
+    // Colliders
     [SerializeField] BoxCollider2D boxCollider1;
     [SerializeField] BoxCollider2D boxCollider2;
-    [SerializeField] CapsuleCollider2D circleCollider1;
-    [SerializeField] CapsuleCollider2D circleCollider2;
-    [SerializeField] GameObject BGWhite1;
-    [SerializeField] GameObject BGWhite2;
-    [SerializeField] GameObject BGBlack1;
-    [SerializeField] GameObject BGBlack2;
+    [SerializeField] TilemapCollider2D tilemapCollider1;
+    [SerializeField] TilemapCollider2D tilemapCollider2;
+    [SerializeField] CapsuleCollider2D cCollider1;
+    [SerializeField] CapsuleCollider2D cCollider2;
+    // Walls & Grounds
     [SerializeField] GameObject GroundWandB;
     [SerializeField] GameObject GroundBandW;
     [SerializeField] GameObject Walls1;
+    [SerializeField] GameObject Walls2;
+    // Audio
     [SerializeField] AudioClip switchon;
     AudioSource audioSource;
-    [SerializeField] GameObject Walls2;
+    // BGS
+    [SerializeField] Tilemap BG1;
+    [SerializeField] Tilemap BG2;
+    // Death
     [SerializeField] GameObject Death1;
     [SerializeField] GameObject Death2;
-    [SerializeField] GameObject decowhite1;
-    [SerializeField] GameObject decowhite2;
-    [SerializeField] GameObject decoblack1;
-    [SerializeField] GameObject decoblack2;
-    [SerializeField] GameObject collidersBox1;
-    [SerializeField] GameObject collidersBox2;
+    // Platforms
+    [SerializeField] Tilemap Platforms1;
+    [SerializeField] Tilemap Platforms2;
+    // Player Sprites
     [SerializeField] SpriteRenderer player1;
     [SerializeField] SpriteRenderer player2;
     [SerializeField] Sprite phase;
@@ -41,19 +44,27 @@ public class PlayerSwitch : MonoBehaviour
     }
     void OnInteract(InputValue value)
     {
-        if (circleCollider1.IsTouchingLayers(LayerMask.GetMask("Interact")) || circleCollider2.IsTouchingLayers(LayerMask.GetMask("Interact")) || boxCollider1.IsTouchingLayers(LayerMask.GetMask("Interact")) || boxCollider2.IsTouchingLayers(LayerMask.GetMask("Interact")))
-        {
-            return;
-        }
         if (value.isPressed)
         {
-            if(box1.activeSelf == true)
+            if(tilemapCollider1.enabled == true)
             {
-                SwitchPlayer1();
+                if (cCollider1.IsTouchingLayers(LayerMask.GetMask("Interact")) || cCollider2.IsTouchingLayers(LayerMask.GetMask("Interact")) || boxCollider1.IsTouchingLayers(LayerMask.GetMask("Interact")) || boxCollider2.IsTouchingLayers(LayerMask.GetMask("Interact")))
+                {
+                    return;
+                }
+                else {
+                    SwitchPlayer1();
+                }
             }
             else
             {
-                SwitchPlayer2();
+                if (cCollider1.IsTouchingLayers(LayerMask.GetMask("Interact")) || cCollider2.IsTouchingLayers(LayerMask.GetMask("Interact")) || boxCollider1.IsTouchingLayers(LayerMask.GetMask("Interact")) || boxCollider2.IsTouchingLayers(LayerMask.GetMask("Interact")))
+                {
+                    return;
+                }
+                else {
+                    SwitchPlayer2();
+                }
             }
         }
     }
@@ -64,22 +75,16 @@ public class PlayerSwitch : MonoBehaviour
         player1Movement.enabled = false;
         player2Position.enabled = false;
         player1Position.enabled = true;
-        box1.SetActive(false);
-        box2.SetActive(true);
-        BGBlack1.SetActive(false);
-        BGWhite1.SetActive(true);
-        BGBlack2.SetActive(true);
-        BGWhite2.SetActive(false);
+        tilemapCollider1.enabled = false;
+        tilemapCollider2.enabled = true;
+        BG1.color = Color.white;
+        BG2.color = Color.black;
         Walls1.SetActive(false);
         Walls2.SetActive(true);
-        decoblack1.SetActive(true);
-        decowhite1.SetActive(false);
-        decoblack2.SetActive(false);
-        decowhite2.SetActive(true);
+        Platforms1.color = Color.black;
+        Platforms2.color = Color.white;
         player1.sprite = phase;
         player2.sprite = normal;
-        collidersBox1.SetActive(false);
-        collidersBox2.SetActive(true);
         GroundWandB.SetActive(false);
         GroundBandW.SetActive(true);
         Death1.SetActive(true);
@@ -92,22 +97,16 @@ public class PlayerSwitch : MonoBehaviour
         player1Movement.enabled = true;
         player2Position.enabled = true;
         player1Position.enabled = false;        
-        box1.SetActive(true);
-        box2.SetActive(false);
-        BGBlack1.SetActive(true);
-        BGWhite1.SetActive(false);
-        BGBlack2.SetActive(false);
-        BGWhite2.SetActive(true);
+        tilemapCollider1.enabled = true;
+        tilemapCollider2.enabled = false;
+        BG1.color = Color.black;
+        BG2.color = Color.white;
         Walls1.SetActive(true);
         Walls2.SetActive(false);
-        decoblack2.SetActive(true);
-        decowhite2.SetActive(false);
-        decoblack1.SetActive(false);
-        decowhite1.SetActive(true);
+        Platforms1.color = Color.white;
+        Platforms2.color = Color.black;
         player1.sprite = normal;
         player2.sprite = phase;
-        collidersBox1.SetActive(true);
-        collidersBox2.SetActive(false);
         GroundWandB.SetActive(true);
         GroundBandW.SetActive(false);
         Death1.SetActive(false);
